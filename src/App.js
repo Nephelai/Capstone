f49d5301a52cd6dee975519e06baf509f89dd311
JeongGeun <40541480+JeongGeun@users.noreply.github.com>
2019-04-09 22:08:23 +0900

    main page+Appbar+gridList

--- src/App.js
+++ src/App.js
@@ -1,163 +1,343 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';import Paper from '@material-ui/core/Paper'
-import Customer from './components/Customer'
-import Table from '@material-ui/core/Table';
-import TableHead from '@material-ui/core/TableHead';
-import TableBody from '@material-ui/core/TableBody';
-import TableRow from '@material-ui/core/TableRow';
-import TableCell from '@material-ui/core/TableCell';
-
+import MailIcon from '@material-ui/icons/Mail';
+import SearchIcon from '@material-ui/icons/Search';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import InputBase from '@material-ui/core/InputBase';
+import ButtonBase from '@material-ui/core/ButtonBase';
 
 const drawerWidth = 240;
 
 const styles = theme => ({
   root: {
-    width:'100%',
-    minwidth:1080
-
+    display: 'flex',
+    width:'100%'
   },
-  paper:{
-    marginLeft:18,
-    marginRight:18
-
-},
-table:{
-    position:"relative",
-    marginLeft:240,
-    width:"77%"
-},
-
-appBar: {
-  zIndex: theme.zIndex.drawer + 1,
-},
-drawer: {
-  width: drawerWidth,
-  flexShrink: 0,
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
   
-},
-drawerPaper: {
-  width: drawerWidth,
-},
-content: {
-  flexGrow: 1,
-  padding: theme.spacing.unit * 3,
-},
-toolbar: theme.mixins.toolbar,
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing.unit,
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%',
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      },
+    },
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 20,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3,
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+  image: {
+    position: 'relative',
+    height: 200,
+    [theme.breakpoints.down('xs')]: {
+      width: '100% !important', // Overrides inline-style
+      height: 100,
+    },
+    '&:hover, &$focusVisible': {
+      zIndex: 1,
+      '& $imageBackdrop': {
+        opacity: 0.15,
+      },
+      '& $imageMarked': {
+        opacity: 0,
+      },
+      '& $imageTitle': {
+        border: '4px solid currentColor',
+      },
+    },
+  },
+  focusVisible: {},
+  imageButton: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: theme.palette.common.white,
+  },
+  imageSrc: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center 40%',
+  },
+  imageBackdrop: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: theme.palette.common.black,
+    opacity: 0.4,
+    transition: theme.transitions.create('opacity'),
+  },
+  imageTitle: {
+    position: 'relative',
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 4}px ${theme.spacing.unit + 6}px`,
+  },
+  imageMarked: {
+    height: 3,
+    width: 18,
+    backgroundColor: theme.palette.common.white,
+    position: 'absolute',
+    bottom: -2,
+    left: 'calc(50% - 9px)',
+    transition: theme.transitions.create('opacity'),
+  },
 });
+const images = [
+  {
+    url: '/hansik.png',
+    title: '한식',
+    width: '40%',
+  },
+  {
+    url: '/joongsick.jpg',
+    title: '중식',
+    width: '30%',
+  },
+  {
+    url: '/일식.jpg',
+    title: '일식',
+    width: '30%',
+  },
+];
 
-class App extends React.Component {
+class PersistentDrawerLeft extends React.Component {
   state = {
-    customers:"",
     open: false,
   };
 
-  componentDidMount(){
-    this.callApi()
-      .then(res=>this.setState({customers:res}))
-      .catch(err=>console.log(err));
-  }
-  callApi=async()=>{
-    const response =await fetch('/categories');
-    const body =await response.json();
-    return body;
-  }
+  handleDrawerOpen = () => {
+    this.setState({ open: true });
+  };
 
+  handleDrawerClose = () => {
+    this.setState({ open: false });
+  };
 
   render() {
-    const { classes } = this.props;
-  
+    const { classes, theme } = this.props;
+    const { open } = this.state;
 
     return (
       <div className={classes.root}>
-      <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
-            ALL-EAT
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <div className={classes.toolbar} />
-        <List>
-          {['한식', '일식', '중식', '양식'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['치킨', '피자', '분식'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-        <Paper position="relative">
-        <Table className={classes.table}>
-          <TableHead>
-          <TableRow>
-            <TableCell>순위</TableCell>
-            <TableCell>가게 이름</TableCell>
-            <TableCell>현재 테이블 수</TableCell>
-            <TableCell>전체 테이블 수</TableCell>
-            <TableCell>대기 시간</TableCell>
-           </TableRow>
-          </TableHead>
-          <TableBody>
-        {
-           this.state.customers? this.state.customers.map(c=>{
-            return(
-              <Customer
-              rank={c.rank}
-              name={c.name}
-              currentTable={c.currentTable}
-              table={c.table}
-              time={c.time}
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          className={classNames(classes.appBar, {
+            [classes.appBarShift]: open,
+          })}
+        >
+          <Toolbar disableGutters={!open}>
+            <IconButton
+              color="inherit"
+              aria-label="Open drawer"
+              onClick={this.handleDrawerOpen}
+              className={classNames(classes.menuButton, open && classes.hide)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" noWrap>
+              ALL-EAT
+            </Typography>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase 
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
               />
+            </div>
+          </Toolbar>
+        </AppBar>
 
-            )
-          }):""
-
-
-
-        }
-        </TableBody>
-        </Table>
-      </Paper> 
+        <Drawer
+          className={classes.drawer}
+          variant="persistent"
+          anchor="left"
+          open={open}
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <div className={classes.drawerHeader}>
+            <IconButton onClick={this.handleDrawerClose}>
+              {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            </IconButton>
+          </div>
+          <Divider />
+          <List>
+            {['한식', '일식', '중식', '양식'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {['치킨', '피자', '분식'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+        
+        <main
+          className={classNames(classes.content, {
+            [classes.contentShift]: open,
+          })}
+        >
+          <div className={classes.drawerHeader} />
+          {images.map(image => (
+        <ButtonBase
+          focusRipple
+          key={image.title}
+          className={classes.image}
+          focusVisibleClassName={classes.focusVisible}
+          style={{
+            width: image.width,
+          }}
+        >
+          <span
+            className={classes.imageSrc}
+            style={{
+              backgroundImage: `url(${image.url})`,
+            }}
+          />
+          <span className={classes.imageBackdrop} />
+          <span className={classes.imageButton}>
+            <Typography
+              component="span"
+              variant="subtitle1"
+              color="inherit"
+              className={classes.imageTitle}
+            >
+              {image.title}
+              <span className={classes.imageMarked} />
+            </Typography>
+          </span>
+        </ButtonBase>
+      ))}
         </main>
       </div>
     );
   }
 }
 
-App.propTypes = {
+PersistentDrawerLeft.propTypes = {
   classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles, { withTheme: true })(App);
+export default withStyles(styles, { withTheme: true })(PersistentDrawerLeft);
---
+++ src/components/Category.js
@@ -0,0 +1,162 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';import Paper from '@material-ui/core/Paper'
+import Customer from './components/Customer'
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    width:'100%',
+    minwidth:1080
+
+  },
+  
+table:{
+    position:"relative",
+    marginLeft:240,
+    width:"77%"
+},
+
+appBar: {
+  zIndex: theme.zIndex.drawer + 1,
+},
+drawer: {
+  width: drawerWidth,
+  flexShrink: 0,
+  
+},
+drawerPaper: {
+  width: drawerWidth,
+},
+content: {
+  flexGrow: 1,
+  padding: theme.spacing.unit * 3,
+},
+toolbar: theme.mixins.toolbar,
+});
+
+class Category extends React.Component {
+  state = {
+    customers:"",
+    open: false,
+  };
+
+  componentDidMount(){
+    this.callApi()
+      .then(res=>this.setState({customers:res}))
+      .catch(err=>console.log(err));
+  }
+  callApi=async()=>{
+    const response =await fetch('/categories');
+    const body =await response.json();
+    return body;
+  }
+
+
+  render() {
+    const { classes } = this.props;
+  
+
+    return (
+      <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            ALL-EAT
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      
+
+
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.toolbar} />
+        <List>
+          {['한식', '일식', '중식', '양식'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['치킨', '피자', '분식'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+
+
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Paper position="relative">
+        <Table className={classes.table}>
+          <TableHead>
+          <TableRow>
+            <TableCell>순위</TableCell>
+            <TableCell>가게 이름</TableCell>
+            <TableCell>현재 테이블 수</TableCell>
+            <TableCell>전체 테이블 수</TableCell>
+            <TableCell>대기 시간</TableCell>
+           </TableRow>
+          </TableHead>
+          <TableBody>
+        {
+           this.state.customers? this.state.customers.map(c=>{
+            return(
+              <Customer
+              rank={c.rank}
+              name={c.name}
+              currentTable={c.currentTable}
+              table={c.table}
+              time={c.time}
+              />
+
+            )
+          }):""
+        }
+        </TableBody>
+        </Table>
+      </Paper> 
+        </main>
+    
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(Category);
