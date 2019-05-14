1194c6a3cf5f2461ebb647342c11efea4cef4ba0
cheshiredog <jaeho406@gmail.com>
2019-05-14 16:13:16 +0900

    Change Structure

--- backend/src/main/java/triplej/capstone/CapstoneApplication.java
+++ backend/src/main/java/triplej/capstone/CapstoneApplication.java
@@ -2,12 +2,13 @@ package triplej.capstone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy
 @SpringBootApplication
 public class CapstoneApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CapstoneApplication.class, args);
 	}
-
 }
