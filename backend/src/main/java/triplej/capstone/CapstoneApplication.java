de8f43f869e69e4ee039d9219e980b2cb8b8194f
cheshiredog <jaeho406@gmail.com>
2019-05-14 15:52:30 +0900

    Change All structure

--- backend/src/main/java/triplej/capstone/CapstoneApplication.java
+++ backend/src/main/java/triplej/capstone/CapstoneApplication.java
@@ -1,15 +1,13 @@
-package main.java.triplej.capstone;
+package triplej.capstone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@EnableAspectJAutoProxy
 @SpringBootApplication
 public class CapstoneApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CapstoneApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneApplication.class, args);
+	}
 
 }
--- backend/src/main/java/triplej/capstone/entities/RestaurantsRepository.java
+++
@@ -1,7 +0,0 @@
-package main.java.triplej.capstone.entities;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// 기본적인 JPA Service 제공 ex> save
-public interface RestaurantsRepository extends JpaRepository<Restaurants, Long> {
-}
