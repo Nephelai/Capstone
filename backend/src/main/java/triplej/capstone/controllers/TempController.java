77777918edbd4076dde1ae35b87c3773c9cc711f
cheshiredog <jaeho406@gmail.com>
2019-05-15 22:39:44 +0900

    Cross Origin 해결

--- backend/src/main/java/triplej/capstone/controllers/TempController.java
+++ backend/src/main/java/triplej/capstone/controllers/TempController.java
@@ -2,12 +2,14 @@ package triplej.capstone.controllers;
 
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import triplej.capstone.dtos.RestaurantResponseDto;
 import triplej.capstone.services.RestaurantsListService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @AllArgsConstructor
 public class TempController {
