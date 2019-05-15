40a07bae6652f72d26d359959ec6726e0b478273
cheshiredog <jaeho406@gmail.com>
2019-05-15 21:45:03 +0900

    gitignore 수정

--- backend/src/main/java/triplej/capstone/controllers/MainController.java
+++ backend/src/main/java/triplej/capstone/controllers/MainController.java
@@ -1,4 +1,14 @@
 package triplej.capstone.controllers;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@AllArgsConstructor
+@Controller
 public class MainController {
+    @GetMapping("/")
+    public String home() {
+        return "index.html";
+    }
 }
