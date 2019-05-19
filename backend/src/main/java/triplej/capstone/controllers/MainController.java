cf1bd7b74a210b90e3e607aac5d29ab1b666b2a9
JeongGeun <40541480+JeongGeun@users.noreply.github.com>
2019-05-19 14:00:59 +0900

    Feature/front communication #21 (#28)
    * Make Simple Service
    
    * Test까지 확인
    Simple Request 끝
    
    * 1. 프론트엔드 폴더 접속
    2. cmd창 열기
    3. npm run build
    4. build디렉토리 만들어지면서 안에 index.html
    
    * last commit

--- backend/src/main/java/triplej/capstone/controllers/MainController.java
+++ backend/src/main/java/triplej/capstone/controllers/MainController.java
@@ -1,14 +1,18 @@
 package triplej.capstone.controllers;
 
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.stereotype.Controller;
+import triplej.capstone.dtos.RestaurantResponseDto;
 
-@AllArgsConstructor
+
+@CrossOrigin(origins = "*")
 @Controller
+@AllArgsConstructor
 public class MainController {
     @GetMapping("/")
-    public String home() {
-        return "index.html";
+    public String main() {
+        return "index";
     }
 }
