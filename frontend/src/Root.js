8b9edc27867ff5d7ca784aeb4a418342336dca67
JeongGeun <40541480+JeongGeun@users.noreply.github.com>
2019-05-20 21:10:48 +0900

    naver api test

--- frontend/src/Root.js
+++ frontend/src/Root.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import  Main  from './Main';
 import Category from './components/Category';
+import Modal from './components/Modal'
 import { BrowserRouter } from 'react-router-dom';
 
 class Root extends Component {
@@ -10,6 +11,7 @@ class Root extends Component {
          <BrowserRouter>
                 <Route exact path="/" component={Main}/>
                 <Route path="/categories" component={Category}/>
+                <Route path="/modal" component={Modal}/>    
         </BrowserRouter>
         );
     }
