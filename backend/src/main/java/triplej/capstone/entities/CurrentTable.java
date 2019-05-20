903cef6977d8e899802af85eb61a1377057489db
cheshiredog <jaeho406@gmail.com>
2019-05-20 20:53:02 +0900

    no message

--- backend/src/main/java/triplej/capstone/entities/CurrentTable.java
+++ backend/src/main/java/triplej/capstone/entities/CurrentTable.java
@@ -15,10 +15,14 @@ public class CurrentTable {
     private long id;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     @OneToOne(targetEntity = Restaurants.class)
 =======
     @OneToOne(targetEntity = Restaurants.class, fetch = FetchType.EAGER, cascade = CascadeType.ALL)
 >>>>>>> 0aa7e60c7d7d91f25169e2aa27b51db2543e0c45
+=======
+    @OneToOne(targetEntity = Restaurants.class, fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+>>>>>>> Sample Data Insert
     @JoinColumn(name = "restaurants_id")
     private long restaurant_id;
 
