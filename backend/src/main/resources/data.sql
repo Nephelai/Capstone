3e70a559fd5cf70741f8c917b1273d765a1caa7d
wogns1026 <sksmsljh1026@naver.com>
2019-05-31 20:00:03 +0900

    no message

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -3,7 +3,7 @@ alter table restaurants auto_increment = 1;
 insert into restaurants (category) values ('ChineseFood');
 insert into chinese_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng) values ('인하각', '01000000001', 20, 1, 3, 37.450995, 126.658060);
 insert into restaurants (category) values ('ChineseFood');
-insert into chinese_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng) values ('짜장전설', '01000000002', 21, 2, 4, 37.452622, 126.657495);
+insert into chinese_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng) values ('짜장전설', '01080302438', 12, 2, 4, 37.452622, 126.657495);
 insert into restaurants (category) values ('ChineseFood');
 insert into chinese_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng) values ('취엔', '01000000003', 10, 3, 5, 37.451528, 126.656782);
 
@@ -17,20 +17,20 @@ insert into japanese_food (name, phone_number, total_table, restaurant_id, curre
 insert into restaurants (category) values ('KoreanFood');
 insert into korean_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng) values ('야우리밥먹자', '01000000021', 20, 7, 7, 37.451269, 126.658027);
 insert into restaurants (category) values ('KoreanFood');
-insert into korean_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng) values ('경동부추삽겹살', '01000000022', 30, 20, 10, 37.451699, 126.656294);
+insert into korean_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng) values ('경동부추삽겹살', '01000000022', 30, 8, 10, 37.451699, 126.656294);
 insert into restaurants (category) values ('KoreanFood');
-insert into korean_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng) values ('고수찜닭', '01000000023', 30, 8, 10, 37.471685, 126.655199);
+insert into korean_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng) values ('고수찜닭', '01000000023', 30, 9, 10, 37.471685, 126.655199);
 insert into restaurants (category) values ('KoreanFood');
-insert into korean_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng) values ('밥풀', '01000000024', 12, 8, 5, 37.451914, 126.656188);
+insert into korean_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng) values ('밥풀', '01000000024', 12, 10, 5, 37.451914, 126.656188);
 
 insert into restaurants (category) values ('WesternFood');
-insert into western_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng) values ('서가엔쿡', '01000000031', 40, 9, 6, 37.448470, 126.649347);
+insert into western_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng) values ('서가엔쿡', '01000000031', 40, 11, 6, 37.448470, 126.649347);
 insert into restaurants (category) values ('WesternFood');
-insert into western_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng) values ('챕터윈스테이크', '01000000032', 20, 10, 8, 37.451914, 126.656188);
+insert into western_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng) values ('챕터윈스테이크', '01000000032', 20, 12, 8, 37.451914, 126.656188);
 insert into restaurants (category) values ('WesternFood');
-insert into western_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng) values ('서브웨이', '01000000033', 10, 11, 2, 37.449679, 126.656547);
+insert into western_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng) values ('서브웨이', '01000000033', 10, 13, 2, 37.449679, 126.656547);
 insert into restaurants (category) values ('WesternFood');
-insert into western_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng) values ('맘스터치', '01000000034', 16, 12, 1, 37.451735, 126.657368);
+insert into western_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng) values ('맘스터치', '01000000034', 16, 14, 1, 37.451735, 126.657368);
 
 insert into restaurants (category) values ('BoonsikFood');
-insert into boonsik_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng) values ('떡볶이에 반한 닭', '01000000041', 40, 9, 4, 37.451153, 126.659023);
+insert into boonsik_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng) values ('떡볶이에 반한 닭', '01000000041', 40, 15, 4, 37.451153, 126.659023);
