04c92b3e512c2134e25d44f69cc92aeaf1746e52
cheshiredog <jaeho406@gmail.com>
2019-05-31 21:08:19 +0900

    Email 처리

---
+++ backend/src/main/java/triplej/capstone/controllers/EmailController.java
@@ -0,0 +1,21 @@
+package triplej.capstone.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import triplej.capstone.services.EmailService;
+
+@RestController
+public class EmailController {
+    @Autowired
+    JavaMailSender javaMailSender;
+
+    @PostMapping("/email")
+    public String emailSend() {
+        EmailService emailService = new EmailService();
+        emailService.setJavaMailSender(javaMailSender);
+        emailService.sendSimpleMessage("jaeho618@naver.com", "제목","내용");
+        return "success";
+    }
+}
