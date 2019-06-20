99a4bac269802196422a4c97d7693c9658c10db2
cheshiredog <jaeho406@gmail.com>
2019-06-20 14:00:50 +0900

    이메일 응답 처리

--- backend/src/main/java/triplej/capstone/dtos/RestaurantEmailDto.java
+++ backend/src/main/java/triplej/capstone/dtos/RestaurantEmailDto.java
@@ -1,4 +1,15 @@
 package triplej.capstone.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
 public class RestaurantEmailDto {
+    private String id;
+    private String email;
 }
--- backend/src/main/java/triplej/capstone/services/FindRestaurantsInfoService.java
+++ backend/src/main/java/triplej/capstone/services/FindRestaurantsInfoService.java
@@ -1,4 +1,53 @@
 package triplej.capstone.services;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import triplej.capstone.dtos.RestaurantEmailDto;
+import triplej.capstone.entities.*;
+
+import java.util.Optional;
+
+@AllArgsConstructor
+@Service
 public class FindRestaurantsInfoService {
+    private RestaurantsRepository restaurantsRepository;
+    private ChineseFoodRepository chineseFoodRepository;
+    private JapaneseFoodRepository japaneseFoodRepository;
+    private KoreanFoodRepository koreanFoodRepository;
+    private WesternFoodRepository westernFoodRepository;
+    private BoonsikFoodRepository boonsikFoodRepository;
+
+    private RestaurantEmailDto restaurantEmailDto;
+
+    public RestaurantEmailDto findEmailById(String id) {
+        Optional<Restaurants> tmpRestaurants = restaurantsRepository.findById(Long.parseLong(id));
+        String tmpCategory = tmpRestaurants.get().getCategory();
+        if(("ChineseFood").equals(tmpCategory)) {
+            Optional<ChineseFood> tmpFood = chineseFoodRepository.findById(Long.parseLong(id));
+            restaurantEmailDto.setId(id);
+            restaurantEmailDto.setEmail(tmpFood.get().getPhoneNumber());
+        }
+        else if(("JapaneseFood").equals(tmpCategory)) {
+            Optional<JapaneseFood> tmpFood = japaneseFoodRepository.findById(Long.parseLong(id));
+            restaurantEmailDto.setId(id);
+            restaurantEmailDto.setEmail(tmpFood.get().getPhoneNumber());
+        }
+        else if(("KoreanFood").equals(tmpCategory)) {
+            Optional<KoreanFood> tmpFood = koreanFoodRepository.findById(Long.parseLong(id));
+            restaurantEmailDto.setId(id);
+            restaurantEmailDto.setEmail(tmpFood.get().getPhoneNumber());
+        }
+        else if(("WesternFood").equals(tmpCategory)) {
+            Optional<WesternFood> tmpFood = westernFoodRepository.findById(Long.parseLong(id));
+            restaurantEmailDto.setId(id);
+            restaurantEmailDto.setEmail(tmpFood.get().getPhoneNumber());
+        }
+        else if(("BoonsikFood").equals(tmpCategory)) {
+            Optional<BoonsikFood> tmpFood = boonsikFoodRepository.findById(Long.parseLong(id));
+            restaurantEmailDto.setId(id);
+            restaurantEmailDto.setEmail(tmpFood.get().getPhoneNumber());
+        }
+
+        return restaurantEmailDto;
+    }
 }
