989a0d95e72f8d82216080a6bfec78dc63075df3
wogns1026 <sksmsljh1026@naver.com>
2019-06-23 22:22:12 +0900

    data 추가

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -34,3 +34,69 @@ insert into western_food (name, phone_number, total_table, restaurant_id, curren
 
 insert into restaurants (category) values ('BoonsikFood');
 insert into boonsik_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('떡볶이에 반한 닭', 'geun5310@naver.com', 40, 15, 0, 37.451153, 126.659023, 1320000);
+
+insert into restaurants (category) values ('BoonsikFood');
+insert into boonsik_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('청년다방', 'geun5310@naver.com', 10, 16, 0, 37.451635, 126.657617, 60000*45);
+insert into restaurants (category) values ('BoonsikFood');
+insert into boonsik_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('명동손만두', 'geun5310@naver.com', 20, 17, 0, 37.451094, 126.659214, 60000*30);
+insert into restaurants (category) values ('BoonsikFood');
+insert into boonsik_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('계속쪼르면', 'geun5310@naver.com', 25, 18, 0, 37.450529, 126.65958, 60000*25);
+insert into restaurants (category) values ('BoonsikFood');
+insert into boonsik_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('신전떡볶이', 'geun5310@naver.com', 20, 19, 0, 37.452216, 126.656127, 60000*35);
+
+insert into restaurants (category) values ('JapaneseFood');
+insert into japanese_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('하꼬멘', 'geun5310@naver.com', 10, 20, 0, 37.451062, 126.658073, 60000*25);
+insert into restaurants (category) values ('JapaneseFood');
+insert into japanese_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('이치라멘', 'geun5310@naver.com', 30, 21, 0, 37.451821, 126.655811, 60000*25);
+insert into restaurants (category) values ('JapaneseFood');
+insert into japanese_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('덴세츠스시', 'geun5310@naver.com', 25, 22, 0, 37.452264, 126.657367, 60000*40);
+insert into restaurants (category) values ('JapaneseFood');
+insert into japanese_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('가메이', 'geun5310@naver.com', 25, 23, 0, 37.451669, 126.656829, 60000*40);
+
+insert into restaurants (category) values ('ChineseFood');
+insert into chinese_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('인하성', 'sksmsljh1026@naver.com', 25, 24, 0, 37.450966, 126.658199, 60000*25);
+insert into restaurants (category) values ('ChineseFood');
+insert into chinese_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('인하반점', 'sksmsljh1026@naver.com', 25, 25, 0, 37.451018, 126.657723, 60000*25);
+insert into restaurants (category) values ('ChineseFood');
+insert into chinese_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('적성루', 'sksmsljh1026@naver.com', 25, 26, 0, 37.451952, 126.654370, 60000*25);
+insert into restaurants (category) values ('ChineseFood');
+insert into chinese_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('니뽕내뽕', 'sksmsljh1026@naver.com', 20, 27, 0, 37.452102, 126.656426, 60000*35);
+
+insert into restaurants (category) values ('KoreanFood');
+insert into korean_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('시내비골', 'geun5310@naver.com', 20, 28, 0, 37.451062, 126.658073, 60000*25);
+insert into restaurants (category) values ('KoreanFood');
+insert into korean_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('맛사랑', 'geun5310@naver.com', 20, 29, 0, 37.451162, 126.657834, 60000*25);
+insert into restaurants (category) values ('KoreanFood');
+insert into korean_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('청진동', 'geun5310@naver.com', 30, 30, 0, 37.451108, 126.658592, 60000*30);
+insert into restaurants (category) values ('KoreanFood');
+insert into korean_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('곤지암할매순대국', 'geun5310@naver.com', 20, 31, 0, 37.450915, 126.658145, 60000*30);
+insert into restaurants (category) values ('KoreanFood');
+insert into korean_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('내찜닭', 'geun5310@naver.com', 25, 32, 0, 37.451481, 126.656171, 60000*45);
+insert into restaurants (category) values ('KoreanFood');
+insert into korean_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('신촌설렁탕', 'geun5310@naver.com', 25, 33, 0, 37.451656, 126.655916, 60000*35);
+insert into restaurants (category) values ('KoreanFood');
+insert into korean_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('진국', 'geun5310@naver.com', 30, 34, 0, 37.451765, 126.656481, 60000*35);
+insert into restaurants (category) values ('KoreanFood');
+insert into korean_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('종가순대', 'geun5310@naver.com', 30, 35, 0, 37.451552, 126.658110, 60000*35);
+
+insert into restaurants (category) values ('WesternFood');
+insert into western_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('왕돈까스', 'geun5310@naver.com', 30, 36, 0, 37.452058, 126.658462, 60000*25);
+insert into restaurants (category) values ('WesternFood');
+insert into western_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('고베규카츠', 'geun5310@naver.com', 15, 37, 0, 37.451159, 126.657579, 60000*45);
+insert into restaurants (category) values ('WesternFood');
+insert into western_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('버거앤프라이즈', 'geun5310@naver.com', 40, 38, 0, 37.451629, 126.656455, 60000*25);
+insert into restaurants (category) values ('WesternFood');
+insert into western_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('시카고피자', 'geun5310@naver.com', 25, 39, 0, 37.451350, 126.656785, 60000*38);
+insert into restaurants (category) values ('WesternFood');
+insert into western_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('파스타앤펍', 'geun5310@naver.com', 15, 40, 0, 37.451647, 126.656572, 60000*40);
+insert into restaurants (category) values ('WesternFood');
+insert into western_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('한스델리', 'geun5310@naver.com', 20, 41, 0, 37.451180, 126.657147, 60000*30);
+insert into restaurants (category) values ('WesternFood');
+insert into western_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('커리야', 'geun5310@naver.com', 30, 42, 0, 37.451866, 126.654802, 60000*50);
+insert into restaurants (category) values ('WesternFood');
+insert into western_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('셰프카레', 'geun5310@naver.com', 25, 43, 0, 37.451330, 126.657162, 60000*45);
+insert into restaurants (category) values ('WesternFood');
+insert into western_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('고양이카레', 'geun5310@naver.com', 20, 44, 0, 37.451686, 126.656313, 60000*30);
+
+insert into restaurants (category) values ('KoreanFood');
+insert into korean_food (name, phone_number, total_table, restaurant_id, current_table, lat, lng, cut_line) values ('시골집', 'geun5310@naver.com', 30, 45, 0, 37.452020, 126.657027, 60000*25);
